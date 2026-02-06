7b62f938f88fc90442dd2ad8f7738976909d1d14
Calvin Tallent <calvintallentspam@gmail.com>
2026-02-06 12:14:52 -0800

    Test of authentication system.

--- frontend/src/routes/test/+page.svelte
+++ frontend/src/routes/test/+page.svelte
@@ -1,1 +1,17 @@
-This is a test of another page.
+<script>
+	import { isAuthenticated, user } from '$lib/stores/auth';
+</script>
+
+<p>This is a test of another page.</p>
+<br /> <br />
+
+{#if $isAuthenticated && $user}
+	<div>
+		<span>User name: {$user.name}</span><br />
+		<span>User email: {$user.email}</span><br />
+	</div>
+{:else}
+	<div class="action-card">
+		Go to <a href="/">home</a>.
+	</div>
+{/if}
